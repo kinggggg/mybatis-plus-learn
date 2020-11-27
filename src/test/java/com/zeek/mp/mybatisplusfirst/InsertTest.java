c2c3e37c80e18103d6e084c642cb8f1f72737410
liweibo03 <liweibo03@kuaishou.com>
2020-11-27 20:06:47 +0800

    插入

---
+++ src/test/java/com/zeek/mp/mybatisplusfirst/InsertTest.java
@@ -0,0 +1,40 @@
+package com.zeek.mp.mybatisplusfirst;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.zeek.mp.mybatisplusfirst.dao.UserMapper;
+import com.zeek.mp.mybatisplusfirst.entity.User;
+import com.zeek.mp.mybatisplusfirst.service.UserService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InsertTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void insert1() {
+
+        User user = new User();
+        user.setName("刘明强");
+        user.setAge(31);
+        user.setManagerId(1088248166370832385l);
+        user.setCreateTime(LocalDateTime.now());
+
+        userMapper.insert(user);
+
+
+    }
+}
