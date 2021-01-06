ac3081da7dff132398d43a1046d7b6c0f0f68fe1
liweibo03 <liweibo03@kuaishou.com>
2021-01-06 20:14:05 +0800

    like查询 between and 查询 order by 升序asc 降序 desc查询 likeRight模糊匹配

--- src/test/java/com/zeek/mp/mybatisplusfirst/InsertTest.java
+++ src/test/java/com/zeek/mp/mybatisplusfirst/InsertTest.java
@@ -14,7 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.zeek.mp.mybatisplusfirst.dao.UserMapper;
 import com.zeek.mp.mybatisplusfirst.entity.User;
 import com.zeek.mp.mybatisplusfirst.service.UserService;
@@ -65,9 +67,61 @@ public class InsertTest {
         users.forEach(System.out::println);
     }
 
+    /**
+     *  名字中包含雨并且年龄小于40
+     *  name like '%雨%' and age < 40
+     */
     @Test
-    public void selectAll() {
-        List<User> list = userMapper.selectList(null);
-        list.forEach(System.out::println);
+    public void selectByWrapper() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        // 也可以通过下面的方式定义queryWrapper
+//        queryWrapper = Wrappers.query();
+
+        // MP中的like已经自动包含了%%
+        queryWrapper.like("name", "雨");
+        queryWrapper.lt("age", 40);
+        // 也可以通过链式调用的方式进行
+//        queryWrapper.like("name", "雨").lt("age", 40);
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    /**
+     * 名字总包含雨并且年龄大于等于20并且小于等于40并且email不为空
+     * name like '%雨%' and age >= 20 and age <=40 and email is not null
+     * name like '%雨%' and age between 20 and 40 and email is not null
+     */
+    @Test
+    public void selectByWrapper2() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper();
+        //第一种查询方式
+//        queryWrapper.like("name", "雨")
+//                .ge("age", 20)
+//                .le("age", 40)
+//                .isNotNull("email");
+        // 第二种查询方式
+        queryWrapper.like("name", "雨")
+                .between("age", 20, 40)
+                .isNotNull("email");
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+    }
+
+    /**
+     * 名字为王姓或者年龄大于等于25, 按照年龄降序排列, 年龄相同按照id升序排列
+     * where name like '%王%' or age >=25 order by age desc, id asc
+     */
+    @Test
+    public void selectByWrapper3() {
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        // 注意: 默认的情况不同的查询条件之间是通过and连接的, 如果为or的话需要显示指定
+        queryWrapper.likeRight("name", "雨")
+                .or().ge("age", 25)
+                .orderByDesc("age")
+                .orderByAsc("id");
+
+        List<User> users = userMapper.selectList(queryWrapper);
+        users.forEach(System.out::println);
+
     }
 }
