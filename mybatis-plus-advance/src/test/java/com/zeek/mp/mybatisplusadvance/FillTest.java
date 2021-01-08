372ed3470bb075b3ca8dfd59b27b789e06d5e4e2
liweibo03 <liweibo03@kuaishou.com>
2021-01-08 23:41:16 +0800

    利用MP进行字段的自动填充

---
+++ mybatis-plus-advance/src/test/java/com/zeek/mp/mybatisplusadvance/FillTest.java
@@ -0,0 +1,46 @@
+package com.zeek.mp.mybatisplusadvance;
+
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.zeek.mp.mybatisplusadvance.dao.UserMapper;
+import com.zeek.mp.mybatisplusadvance.entity.User;
+
+/**
+ * @author liweibo03 <liweibo03@kuaishou.com>
+ * Created on 2021-01-08
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FillTest {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    public void insert() {
+        User user = new User();
+        user.setName("小红");
+        user.setAge(34);
+
+        int rows = userMapper.insert(user);
+        System.out.println("影响行数: " + rows);
+    }
+
+    @Test
+    public void update() {
+        User user = new User();
+        user.setId(1088248166370832385L);
+        user.setAge(28);
+
+        int rows = userMapper.updateById(user);
+        System.out.println(rows);
+    }
+
+}
